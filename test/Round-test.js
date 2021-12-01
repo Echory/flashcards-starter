21cc94ec0987a9627f9e4e47bbd0df91b0d7ad48
Emma Chory <emmachory@gmail.com>
2021-12-01 09:09:59 -0700

    Write return card test and function

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const expect = chai.expect;
 
@@ -10,7 +11,7 @@ describe('Round', function() {
     expect(Round).to.be.a('function');
   });
 
-  it('should be an instance of a Turn', function() {
+  it('should be an instance of a Round', function() {
     const round = new Round();
     expect(round).to.be.an.instanceOf(Round);
   });
@@ -19,5 +20,20 @@ describe('Round', function() {
     const deck = new Deck([]);
     const round = new Round(deck);
     expect(round.deck).to.be.an.instanceOf(Deck);
+  });
+
+  it('should return current card', function() {
+    const card = new Card(1,
+    "What allows you to define a set of related information using key-value pairs?",
+    ["object", "array", "function"],
+    "object");
+    const deck = new Deck([card]);
+    const round = new Round(deck);
+    const currentCard = round.returnCurrentCard();
+    expect(currentCard).to.equal(card);
+  });
+
+  it('should create a new instance of turn', function() {
+
   });
 })
