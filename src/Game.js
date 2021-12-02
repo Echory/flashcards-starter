fbb123987fb33e8dc558ffea88a1319d9e02b4ea
Emma Chory <emmachory@gmail.com>
2021-12-02 16:17:40 -0700

    Write tests and functions for game

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,14 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0;
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +19,19 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
+
+  start() {
+    let cards = prototypeQuestions.map((element) => {
+      const card = new Card(element["id"], element["question"], element["answers"], element["correctAnswer"]);
+      return card;
+    });
+    let deck = new Deck(cards);
+    let round = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
+  
 }
+
 
 module.exports = Game;
